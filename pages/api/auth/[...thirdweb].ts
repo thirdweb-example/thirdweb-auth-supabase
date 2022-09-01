b3c5a21c04547ce98fdd3b9a3cd080af0fd26dbd
adam-maj <mr.adam.maj@gmail.com>
2022-08-31 17:17:40 -0700

    Add supabase functionality to backend

--- pages/api/auth/[...thirdweb].ts
+++ pages/api/auth/[...thirdweb].ts
@@ -1,3 +1,41 @@
-import { ThirdwebAuthHandler } from "../../../auth.config";
+import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
+import { createClient } from '@supabase/supabase-js';
 
-export default ThirdwebAuthHandler()
+const supabase = createClient(
+  process.env.SUPABASE_URL || "", 
+  process.env.SUPABASE_KEY || ""
+);
+
+export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
+  privateKey: process.env.ADMIN_PRIVATE_KEY || "",
+  domain: "localhost:3000",
+  callbacks: {
+    login: async (address: string) => {
+      const { data: user } = 
+        await supabase
+          .from("user")
+          .select("*")
+          .eq("address", address)
+          .single();
+
+      if (!user) {
+        await supabase
+          .from('users')
+          .insert({ address })
+          .single();
+      }
+    },
+    user: async (address: string) => {
+      const { data: user } = 
+        await supabase
+          .from("user")
+          .select("*")
+          .eq("address", address)
+          .single();
+
+      return { ...user };
+    },
+  }
+});
+
+export default ThirdwebAuthHandler();
