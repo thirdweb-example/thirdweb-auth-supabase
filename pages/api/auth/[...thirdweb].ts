fa5458ef187e7f4aa6bb67106fef4378a90e80f0
adam-maj <mr.adam.maj@gmail.com>
2022-09-09 11:30:10 -0700

    Update domain to example.com

--- pages/api/auth/[...thirdweb].ts
+++ pages/api/auth/[...thirdweb].ts
@@ -1,45 +1,43 @@
 import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL || "", 
+  process.env.SUPABASE_URL || "",
   process.env.SUPABASE_SERVICE_ROLE || ""
 );
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
   privateKey: process.env.ADMIN_PRIVATE_KEY || "",
-  domain: "localhost:3000",
+  domain: "example.com",
   callbacks: {
     login: async (address: string) => {
-      const { data: user } = 
-        await supabase
-          .from("users")
-          .select("*")
-          .eq("address", address.toLowerCase())
-          .single()
+      const { data: user } = await supabase
+        .from("users")
+        .select("*")
+        .eq("address", address.toLowerCase())
+        .single();
 
       if (!user) {
         const res = await supabase
-          .from('users')
+          .from("users")
           .insert({ address: address.toLowerCase() })
           .single();
-        
+
         if (res.error) {
-          throw new Error("Failed to create user!")
+          throw new Error("Failed to create user!");
         }
       }
     },
     user: async (address: string) => {
-      const { data: user } = 
-        await supabase
-          .from("users")
-          .select("*")
-          .eq("address", address.toLowerCase())
-          .single()
+      const { data: user } = await supabase
+        .from("users")
+        .select("*")
+        .eq("address", address.toLowerCase())
+        .single();
 
       return user;
     },
-  }
+  },
 });
 
 export default ThirdwebAuthHandler();
